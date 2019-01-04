0fb8081cd7ef356d6a8226de9c5ac08ee4c67902
Artem Alagizov <artem.alagizov@asml.com>
2019-01-04 08:24:53 +0100

    added cell selection handling for sorting scenario

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {assoc, flatten} from 'ramda';
+import {assoc, flatten, isNil} from 'ramda';
 
 import 'react-tabulator/lib/styles.css';
 import 'react-tabulator/lib/css/tabulator.min.css'; // theme
@@ -10,6 +10,14 @@ import {ReactTabulator} from 'react-tabulator';
 
 class App extends Component {
   defaultBorder = '0.2em solid';
+  selectedCellBorderStyle = `${this.defaultBorder} #336699`;
+  notSelectedCellBorderStyle = `${this.defaultBorder} transparent`;
+
+  state = {
+    selectedCellRow: null,
+    selectedKpiCell: null,
+    selectedKpiCellField: null
+  };
 
   render() {
     const cellFormatter = cell => {
@@ -20,29 +28,45 @@ class App extends Component {
         cell.getElement().style.backgroundColor = '#FF3333';
       }
 
-      cell.getElement().style.border = `${this.defaultBorder} transparent`;
+      cell.getElement().style.border = isCurrentCellSelected(cell) ? this.selectedCellBorderStyle : this.notSelectedCellBorderStyle;
 
       return value;
     };
 
+    const isCurrentCellSelected = cell => {
+      if (isNil(this.state.selectedKpiCell)) return false;
+
+      const currentCellRow = cell.getData();
+      const currentCellField = cell.getField();
+      const { selectedKpiCellField, selectedCellRow } = this.state;
+      const isSameRow = selectedCellRow.id === currentCellRow.id;
+      const isSameKey = selectedKpiCellField === currentCellField;
+
+      return isSameKey && isSameRow;
+    };
+
     const cellClick = (event, cell) => {
       const borderCss = cell.getElement().style.border;
-      const selectedCellBorderStyle = `${this.defaultBorder} #336699`;
-      const notSelectedCellBorderStyle = `${this.defaultBorder} transparent`;
-      const currentCellSelected = borderCss !== notSelectedCellBorderStyle;
+      const currentCellSelected = borderCss !== this.notSelectedCellBorderStyle;
       const allCells = flatten(cell.getTable().getRows().map(row => row.getCells()));
 
       // deselect all cells
-      allCells.map(cell => cell.getElement().style.border = notSelectedCellBorderStyle);
+      allCells.map(cell => cell.getElement().style.border = this.notSelectedCellBorderStyle);
 
       // select/deselect current cell
-      cell.getElement().style.border = currentCellSelected ? notSelectedCellBorderStyle : selectedCellBorderStyle;
+      cell.getElement().style.border = currentCellSelected ? this.notSelectedCellBorderStyle : this.selectedCellBorderStyle;
 
       // add current cell to the resulting data
       window.spotfireData = currentCellSelected ? {} : {
         selectedCellRow: cell.getData(),
         selectedKpiCell: cell.getValue()
       };
+
+      this.setState({
+        selectedCellRow: currentCellSelected ? null : cell.getData(),
+        selectedKpiCell: currentCellSelected ? null : cell.getValue(),
+        selectedKpiCellField: currentCellSelected ? null : cell.getField()
+      });
     };
 
     const tooltip = cell => `Threshold value is ${cell.getValue().threshold}`;
